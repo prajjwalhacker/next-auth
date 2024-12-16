d15581b0ebeefc1337570c5328495972cede2615
prajjwalsoni123 <prajjwalsoni123@gmail.com>
2024-12-16 19:48:11 +0530

    mailer html template addition

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -1,40 +1,60 @@
 import nodemailer from 'nodemailer';
 import bcryptjs from 'bcryptjs';
+import User from '@/models/userModel';
 
-export const sendEmail = async ({ email, emailType, userId }) => {
+export const sendEmail = async ({ email, emailType, userId }: { email: string, emailType: string, userId: string }) => {
      try {
 
         if (emailType === 'VERIFY') {
            const salt = await bcryptjs.genSalt(10);
            const hashedToken = await bcryptjs.hash(userId.toString(), salt);
-           
+
+           const currentDate = new Date(); 
+           const nextDay = new Date(currentDate);  
+           nextDay.setDate(currentDate.getDate() + 1); 
+
+           await User.findOneAndUpdate({ email }, {
+            $set: {
+                verifyToken: hashedToken,
+                verifyTokenExpiry: nextDay
+            }
+           })
+
+           const auth = nodemailer.createTransport({
+            service: 'gmail',
+            secure: true, // true for port 465, false for other ports
+            port: 465,
+            auth: {   
+              user: "prajjwalsoni123@gmail.com",
+              pass: "mbqiltnurraaseib",
+            },
+          });
+
+          const receiver = {
+            from: "prajjwalsoni123@gmail.com",
+            to: "shrilaxmi06@gmail.com",    
+            subject: "NODEJS Mail testing",
+            html: `
+    <div style="font-family: Arial, sans-serif; line-height: 1.5; padding: 20px; background-color: #f9f9f9; border: 1px solid #ddd;">
+      <h1 style="color: #333;">Hello, Everyone!</h1>
+      <p style="font-size: 16px; color: #555;">This is a mail regarding <b>Email</b> and <b>Verification</b>.</p>
+      <p style="font-size: 14px; color: #777;">Please verify your email y clicking ${`${process.env.DOMAIN}/verifyEmail?token=${hashedToken}`}</p>
+      <div style="margin-top: 20px; padding: 10px; background-color: #e7f3fe; border: 1px solid #b3d7ff;">
+        <p style="font-size: 14px; color: #0056b3;">If you have any questions, feel free to reply to this email.</p>
+      </div>
+      <p style="font-size: 12px; color: #999;">Thank you for using our service!</p>
+    </div>
+  `, // HTML content 
+          }
+
+          const info = await auth.sendMail(receiver);
+        
+          console.log("Message sent: %s", info.messageId);
+
         }
         else if (emailType ==='RESET') {
             
         }
-        const transporter = nodemailer.createTransport({
-            host: "smtp.forwardemail.net",
-            port: 587,
-            secure: false, // true for port 465, false for other ports
-            auth: {
-              user: "maddison53@ethereal.email",
-              pass: "jn7jnAPss4f63QBp6D",
-            },
-          });
-
-          const info = await transporter.sendMail({
-            from: '"Maddison Foo Koch 👻" <maddison53@ethereal.email>', // sender address
-            to: "bar@example.com, baz@example.com", // list of receivers
-            subject: "Hello ✔", // Subject line
-            text: "Hello world?", // plain text body
-            html: "<b>Hello world?</b>", // html body
-          });
-        
-          console.log("Message sent: %s", info.messageId);
-
-         if (emailType === 'VERIFY') {
-
-         }
      }
      catch (err: any) {
         console.log(err.message);

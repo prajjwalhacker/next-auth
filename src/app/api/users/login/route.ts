c2c6fb1e82b4fa7053ec60a74e0b08a26e39526f
prajjwalsoni123 <prajjwalsoni123@gmail.com>
2024-12-16 21:40:43 +0530

    router changes

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,42 @@
+import { connect } from '@/dbConfig/dbConfig';
+
+import { NextRequest, NextResponse } from 'next/server';
+import User  from '@/models/userModel';
+import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+connect();
+
+export async function POST(request: NextRequest) {
+     try {
+        const { email,  password } = await request.json();
+
+
+        const userObj = await User.findOne({ email });
+
+        if (!userObj) {
+            return NextResponse.json({ message: "user doesnt exists" });
+        }
+        else {
+            const isEqual = await bcryptjs.compare(password, userObj?.password);
+            if (isEqual) {
+                const response = NextResponse.json({
+                    user: userObj
+                });
+                const token = jwt.sign({ id: userObj?._id }, process.env.TOKEN_SECRET!, { algorithm: 'RS256' });
+                response.cookies.set('token', token, {
+                    httpOnly: true
+                });
+                return response;
+            }
+            else {
+                return NextResponse.json({ message: "incorrect password" });
+            }
+        }
+
+     }
+     catch(err:any) {
+        console.log("something went wrong");
+        NextResponse.json({ msg: "something went wrong !" });
+     }
+}

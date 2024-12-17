0716b5ad31231c8283cd8eb94471be712c71caf9
prajjwalsoni123 <prajjwalsoni123@gmail.com>
2024-12-17 13:40:27 +0530

    taiwind css configuraionn changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Navbar from "./_components/Navbar";
 
 export default function Home() {
   return (
-    <div className="bg-primary-black overflow:hidden h-screen">
+    <div className="bg-primary-black  h-screen">
       <Navbar/>
     </div>
   );

10ce6414c8a0840b75e0567158b4db6393bc0aef
prajjwalsoni123 <prajjwalsoni123@gmail.com>
2024-12-17 14:23:37 +0530

    hero compoet added

---
+++ src/app/_components/Hero.tsx
@@ -0,0 +1,12 @@
+'use client';
+import { navVariants } from "@/utils/motion";
+import styles from "@/utils/stylesMap";
+import { motion } from "framer-motion";
+
+const Hero = () => {
+  return (
+    <div>Hero</div>
+  )
+}
+
+export default Hero

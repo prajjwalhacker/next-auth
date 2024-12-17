221aa924945eb1f94980ada0f42893658ae8c164
prajjwalsoni123 <prajjwalsoni123@gmail.com>
2024-12-17 16:19:07 +0530

    framer motion changes

--- src/app/_components/Hero.tsx
+++ src/app/_components/Hero.tsx
@@ -1,11 +1,37 @@
 'use client';
-import { navVariants } from "@/utils/motion";
+import { navVariants, slideIn, staggerContainer, textVariant } from "@/utils/motion";
 import styles from "@/utils/stylesMap";
+import coverI from '../../../public/cover.png';
 import { motion } from "framer-motion";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className={`${styles.yPaddings} sm:pl-16 pl-6`}>
+    <motion.div
+      variants={staggerContainer}
+      initial='hidden'
+      whileInView={'show'}
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+        <div className="flex flex-col justify-center items-center z-10">
+        <motion.h1 variants={textVariant(1.1)} className={styles.heroHeading}>
+           Interview 
+        </motion.h1>
+        <motion.div variants={textVariant(1.2)} className={`${styles.heroHeading} flex flex-row justify-center items-center`}>
+           With AI
+        </motion.div>
+        </div>
+        <motion.div
+         variants={slideIn('right', 'tween', 0.2, 1)}
+         className="relative w-full h-[300px] md-mt-[20px] -mt-[12px]"
+        >
+          <div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] -top-[30px]">
+              <img src={coverI.src} alt="" className="-top-[30px] w-full sm:h-[300px] h-[300px] object-cover rounded-tl-[140px] z-0 relative"/>   
+          </div>  
+        </motion.div>
+    </motion.div>
+    </section>
   )
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Hero from "./_components/Hero";
 import Navbar from "./_components/Navbar";
 
 export default function Home() {
   return (
-    <div className="bg-primary-black  h-screen">
+    <div className="bg-primary-black h-screen overflow-hidden">
       <Navbar/>
+      <Hero/>
     </div>
   );
 }

d8494ea0f293912b989db0b077671f6283b16162
prajjwalsoni123 <prajjwalsoni123@gmail.com>
2024-12-17 13:28:21 +0530

    style chages

--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -1,14 +1,16 @@
 'use client';
 import { navVariants } from "@/utils/motion";
+import styles from "@/utils/stylesMap";
 import { motion } from "framer-motion";
 
 const Navbar = () => (
   <motion.nav 
    variants={navVariants}
    initial='hidden'
+   className={`${styles.xPaddings} py-8 relative`}
    animate='show'
   >
-    navbar
+     <div className=""/>
   </motion.nav>
 );
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Navbar from "./_components/Navbar";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-primary-black overflow:hidden h-screen">
       <Navbar/>
     </div>
   );

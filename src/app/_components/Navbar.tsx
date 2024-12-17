a078c70ea00690278ef95def991aeb367e952d0f
prajjwalsoni123 <prajjwalsoni123@gmail.com>
2024-12-17 14:15:00 +0530

    sigup utton for me

--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -15,9 +15,9 @@ const Navbar = () => (
      <div className="font-extrabold text-[24px] leading-[30px] text-white">
         Mock interview with AI
      </div>
-     <div>
-        Dfsdfsdf
-     </div>
+     <button className="px-6 py-3 bg-white text-primary-black font-semibold rounded-full shadow-md hover:bg-gray-100 hover:shadow-lg transition duration-300 ease-in-out transform hover:-translate-y-1">
+        Sign Up
+     </button>
      </div>
   </motion.nav>
 );
